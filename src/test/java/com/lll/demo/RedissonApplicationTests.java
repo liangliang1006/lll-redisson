ff24226f6db8e6510ea0de198aaa4dec48d81a63
liangliang <liull@xiaoqiedu.cn>
2019-12-11 18:35:58 +0800

    初始化demo

---
+++ src/test/java/com/lll/demo/RedissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lll.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedissonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
